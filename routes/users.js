eaed8b15d625dad4892ae2a66d057c1d7033ce99
mosescode1 <emosescode@gmail.com>
2024-10-07 14:54:29 +0100

    initial commit

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 /* GET users listing. */
-router.post('/signup', userController.createUser);
+router.post('/signup', userController.createUser).get("/", function(req, res,)=>{
+	res.status(200).json({user:true});
+})
 
 module.exports = router;
